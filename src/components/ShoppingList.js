5ce1f49fd081597317b8f369ac8e1f41bc4ecb35
N003AD <diop97tidiane@gmail.com>
2024-08-06 13:33:46 +0000

    props-children

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,6 @@
 import { plantList } from '../datas/plantList'
 import '../styles/ShoppingList.css'
+import CareScale from './CareScale.js'
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -18,8 +19,10 @@ function ShoppingList() {
 			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
 					<li key={plant.id} className='lmj-plant-item'>
+						{plant.isBestSale && <span>🔥</span>}
 						{plant.name}
-						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+						<CareScale careType='water' scaleValue={plant.water} />
+						<CareScale careType='light' scaleValue={plant.light} />
 					</li>
 				))}
 			</ul>
